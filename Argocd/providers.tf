0f9101d468df14827a88283a81d11434b54c5242
Kausar1011 <118176996+Kausar1011@users.noreply.github.com>
2025-08-15 17:52:25 +0530

    Update providers.tf

--- Argocd/providers.tf
+++ Argocd/providers.tf
@@ -1,1 +1,27 @@
+terraform {
+  required_version = ">= 1.4.0"
+  required_providers {
+    aws        = { source = "hashicorp/aws",        version = ">= 5.0" }
+    kubernetes = { source = "hashicorp/kubernetes", version = ">= 2.29" }
+    helm       = { source = "hashicorp/helm",       version = ">= 2.11" }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.this.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.this.token
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.this.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.this.token
+  }
+}
 
