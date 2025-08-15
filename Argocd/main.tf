3b2614f494670c07e8280415e20c2c9809e5609a
Kausar1011 <118176996+Kausar1011@users.noreply.github.com>
2025-08-15 17:51:52 +0530

    Update main.tf

--- Argocd/main.tf
+++ Argocd/main.tf
@@ -1,1 +1,40 @@
+# Get EKS outputs from step2
+data "terraform_remote_state" "eks" {
+  backend = "local"
+  config = {
+    path = "../step2-eks/terraform.tfstate"
+  }
+}
+
+data "aws_eks_cluster" "this" {
+  name = data.terraform_remote_state.eks.outputs.cluster_name
+}
+
+data "aws_eks_cluster_auth" "this" {
+  name = data.terraform_remote_state.eks.outputs.cluster_name
+}
+
+resource "kubernetes_namespace" "argocd" {
+  metadata {
+    name = "argocd"
+  }
+}
+
+resource "helm_release" "argocd" {
+  name       = "argo-cd"
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
+  namespace  = kubernetes_namespace.argocd.metadata[0].name
+  version    = var.argocd_chart_version
+
+  set {
+    name  = "server.service.type"
+    value = "LoadBalancer"
+  }
+
+  set {
+    name  = "server.extraArgs"
+    value = "{--insecure}"
+  }
+}
 
