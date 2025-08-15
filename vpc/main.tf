f165f9491d1e80104c317d91913800d3070787d5
Kausar1011 <118176996+Kausar1011@users.noreply.github.com>
2025-08-15 17:49:47 +0530

    Update main.tf

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,1 +1,18 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = ">= 5.0"
+
+  name = "${var.cluster_name}-vpc"
+  cidr = var.vpc_cidr
+
+  azs            = ["${var.aws_region}a", "${var.aws_region}b"]
+  public_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Project = var.cluster_name
+  }
+}
 
