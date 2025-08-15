ea5130fb30d02ae6c78cd89354a939013eb7f9fd
Kausar1011 <118176996+Kausar1011@users.noreply.github.com>
2025-08-15 17:51:00 +0530

    Update main.tf

--- eks/main.tf
+++ eks/main.tf
@@ -1,1 +1,32 @@
+# Get VPC outputs from step1
+data "terraform_remote_state" "vpc" {
+  backend = "local"
+  config = {
+    path = "../step1-vpc/terraform.tfstate"
+  }
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = ">= 20.0"
+
+  cluster_name    = var.cluster_name
+  cluster_version = var.cluster_version
+
+  vpc_id     = data.terraform_remote_state.vpc.outputs.vpc_id
+  subnet_ids = data.terraform_remote_state.vpc.outputs.public_subnets
+
+  eks_managed_node_groups = {
+    default = {
+      desired_size  = 2
+      min_size      = 2
+      max_size      = 3
+      instance_types = ["t3.medium"]
+    }
+  }
+
+  tags = {
+    Project = var.cluster_name
+  }
+}
 
